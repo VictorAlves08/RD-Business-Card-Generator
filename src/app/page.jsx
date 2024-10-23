a608ec23d88cf6901fc36533ba4a5228f5327b67
VictorAlves08 <victoralvees17@gmail.com>
2024-10-22 23:39:16 -0300

    setup initial project

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+ "use client"
+ 
+import styled from "styled-components";
+
+export default function Home() {
+    return (
+        <div>
+            <Header>Home</Header>
+        </div>
+    );
+}
+
+const Header =  styled.h1`
+    color: red;
+`
