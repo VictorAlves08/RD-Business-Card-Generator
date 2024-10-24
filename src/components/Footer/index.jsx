1f7d84a47ecef2258010b7e3aad7a3b9dfd325a5
VictorAlves08 <victoralvees17@gmail.com>
2024-10-24 04:12:00 -0300

    feat: create footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,25 @@
+import Text from "@/components/Text";
+import colors from "../../styles/colors.js";
+import typography from "../../styles/typography.js";
+import { Container } from "./styles.js";
 
 export default function Footer() {
     return (
-        <div>Footer 2</div>
+        <Container>
+            <Text
+                type="link"
+                text="Política de Privacidade"
+                fontSize={typography.size.sm}
+                fontWeight={typography.weight.bold}
+                color={colors.grayDark}
+                style={{ textDecoration: "underline" }}
+            />
+            <Text
+                text="© 2024 Resultados Digitais"
+                fontSize={typography.size.sm}
+                fontWeight={typography.weight.bold}
+                color={colors.gray}
+            />
+        </Container>
     )
 }
