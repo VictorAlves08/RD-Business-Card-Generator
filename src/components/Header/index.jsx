1c3366009f4158389b2532a2caa1d3ed02c48c46
VictorAlves08 <victoralvees17@gmail.com>
2024-10-24 04:11:38 -0300

    feat: create header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,28 @@
+import React from "react";
+
+import Image from 'next/image'
+import Logo from '../../../public/logo_with_name.svg'
+
+import Text from "@/components/Text";
+
+import colors from "../../styles/colors.js";
+import typography from "../../styles/typography.js";
+import { Container } from "./styles";
 
 export default function Header() {
     return (
-        <div>Header</div>
+        <Container>
+            <Image
+                src={Logo}
+                alt="Logo Resultados Digitais"
+                width={150}
+            />
+            <Text
+                text="Gerador de Cartão de Visita"
+                fontSize={typography.size.lg}
+                fontWeight={typography.weight.bold}
+                color={colors.black}
+            />
+        </Container>
     )
 }
---
+++ src/components/Header/styles.js
@@ -0,0 +1,14 @@
+'use client';
+
+import styled from 'styled-components';
+
+export const Container = styled.header`
+    width: 90%;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    padding: 1.25rem 2rem;
+`;
